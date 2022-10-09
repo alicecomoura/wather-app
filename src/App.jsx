25ef1a657f364f8a25293861789367ebff65d3fe
Alice Coelho <alicecomoura@outlook.com>
2022-10-09 13:29:04 -0300

    initial dos values ​​and responsive style adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import { Card } from './components/card';
+import React, { useState } from "react";
+
+import { fetchData } from "./services/api";
+import { initalData } from "./services/helpers/initial-data";
+import { Card } from "./components/card";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState(initalData);
+
+  const handleSubimit = (event) => {
+    event.preventDefault();
+
+    fetchData(city).then((response) => {
+      setData(response);
+    });
+  };
+
   return (
-    <div className="flex flex-col m-full h-screen items-center justify-center">
-      <form>
-        <input 
-          type="text" 
+    <div className="min-w-[380px] flex flex-col m-full h-screen items-center sm:justify-center p-4">
+      <form onSubmit={handleSubimit} className="fixed bottom-10 w-full flex justify-center p-4 sm:relative">
+        <input
+          type="text"
           placeholder="Cidade"
-          className="p-3 rounded-lg" 
+          className="p-3 rounded-lg w-full flex-1 sm:max-w-[350px]"
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
         />
-        <button 
+        <button
           type="submit"
           className="bg-blue-400 p-3 rounded-lg ml-3 text-white font-bold"
         >
           Pesquisar
         </button>
       </form>
-
-      <Card />
+      <Card data={data} />
     </div>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 
-export const Card = () => {
+export const Card = ({ data }) => {
+  const { location, current } = data;
+
   return (
-    <div className="bg-white p-6 mt-10 rounded-lg shadow-md">
-      <div>
-        <h3 className="text-xl font-bold text-slate-500">Rio de Janeiro</h3>
-        <p className="text-slate-400 text-sm font-medium">Rio de Janeiro, Brasil</p>
-      </div>
-      <div className="font-bold text-slate-700 flex mt-4 mb-2">
-        <span className="text-8xl">27</span>
-        <span className="text-2xl mt-2">°C</span>
+    <div className="bg-white p-6 mt-10 rounded-lg shadow-md min-w-[350px]">
+      <div className="flex justify-around items-center">
+        <div>
+          <h3 className="text-xl font-bold text-slate-500">{location.name}</h3>
+          <p className="text-slate-400 text-sm font-medium">
+            {`${location.region}, ${location.country}`}
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+          <img
+            src={current.condition.icon}
+            alt={`Wather icon: ${current.condition.text}`}
+          />
+          <p className="font-medium text-slate-700">{current.condition.text}</p>
+        </div>
       </div>
 
-      <div className="text-center">
-        <span className="block">icone</span>
-        <p className="font-medium text-slate-700">Nublado</p>
+      <div className="font-bold text-slate-700 flex justify-center mt-4 mb-2">
+        <span className="text-8xl">{current.temp_c}</span>
+        <span className="text-2xl mt-2">°C</span>
       </div>
     </div>
   );
