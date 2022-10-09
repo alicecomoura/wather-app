1ba70c9a3b350595cc0863d4df1e12feabd9c278
Alice Coelho <alicecomoura@outlook.com>
2022-10-09 13:28:14 -0300

    data fetch function

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+const KEY = "76d5ff274fd84ede91e151037220910";
+
+export const fetchData = async (city) => {
+  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+
+  const fetchResponse = await fetch(url);
+  const data = await fetchResponse.json();
+
+  return data;
+};
